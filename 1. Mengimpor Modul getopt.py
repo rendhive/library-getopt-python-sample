fd0f99f3aa1046021eea5200bb593322ba5aa673
rendhive <rendhivecode@gmail.com>
2026-06-21 19:07:26 +0700

    [DOCT] Sample Library Python: 1. Mengimpor Modul getopt.py

---
+++ 1. Mengimpor Modul getopt.py
@@ -0,0 +1,6 @@
+import sys
+import getopt
+
+print("Modul getopt berhasil diimpor.")
+# Fungsi: Mengimpor modul untuk parsing argumen command line.
+# Kondisi: Digunakan sebelum mendefinisikan dan mem-pars argumen.
